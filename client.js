5a170d3b493a98dbee6043c77cab2b27f2f7edce
kristinejensen <kristine.b.jensen@gmail.com>
2017-02-07 22:24:46 -0600

    Update client.js

--- client.js
+++ client.js
@@ -6,5 +6,5 @@ function yourFunction (array, number) { // I'm creating the function on lines 5 
 return array[number] + array[(number + 1)];
 }
 // So, you can see that I did not fully solve the problem.
-//I got stuck on how to return a string beginning with the index number called in the function through the end of the string.
-//Since I got stuck, I decided to solve the first example provided. I know this is not the correct answer for the entire assignment.
+//I got stuck on how to "return a string that combines all of the values starting at the index of the number passed in through the end of the array."
+//Since I got stuck, I decided to solve the first example provided in the assignment. I know this is not the correct answer for the entire assignment.
